75d440ed3a478408fb747bf1f3dc89354a3df0c6
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-01-10 18:16:14 +0500

    Added server side validation

--- action/postAction.js
+++ action/postAction.js
@@ -4,10 +4,28 @@ import { storePost } from "@/lib/posts";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export async function createPost(formData) {
+export async function createPost(prevstate, formData) {
   const title = formData.get("title");
   const image = formData.get("image");
   const content = formData.get("content");
+
+  let errors = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push("Title us required");
+  }
+
+  if (!content || content.trim().length === 0) {
+    errors.push("Content us required");
+  }
+
+  if (!image || image.size === 0) {
+    errors.push("Image is required");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
 
   await storePost({
     imageUrl: "",
@@ -17,6 +35,5 @@ export async function createPost(formData) {
   });
 
   revalidatePath("/feed");
-
   redirect("/feed");
 }
