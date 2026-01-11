0865a1cdd93accf0a8e101e0bf06347d0103366b
Muhammad Shehryar <Shahryartariq57@gmail.com>
2026-01-11 05:22:49 +0500

    Added Metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,16 @@
 import Posts from "@/components/posts";
 import { getPosts } from "@/lib/posts";
 
+export async function generateMetadata(data) {
+  // console.log(data);
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse Our ${numberOfPosts} Posts`,
+    description: "Browser all of our postbox posts"
+  }
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
